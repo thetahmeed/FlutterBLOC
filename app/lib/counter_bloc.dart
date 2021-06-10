9325d161f17fd3a77acb244b01832815b6255d8c
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-06-10 23:26:36 +0600

    Some important keywords

--- app/lib/counter_bloc.dart
+++ app/lib/counter_bloc.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
 
 class CounterBloc {
-  final _streamController = StreamController();
+  // StreamController => [No one knows]
   // Here is some things that you should cons.
   // sink => To add data to the pipe
-  // StreamController => No one knows.
   // stream => To get data from the pipe
   // [Here pipe is the media]
-  // 'int' is the kind of data that we will exchange
+
+  // 1. StreamController: 'int' is the kind of data that we will exchange
   final _streamController = StreamController<int>();
+
+  // 2. Sink:
+  StreamSink<int> get counterSink => _streamController.sink;
+
+  // 3. Stream:
+  Stream<int> get counterStream => _streamController.stream;
 }
