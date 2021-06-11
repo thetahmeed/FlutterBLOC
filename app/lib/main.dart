0b248cef1eba989124bc6a9c4e343ac6c60727d9
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-06-11 10:19:23 +0600

    IGNORE: Just rearranged

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/counter_bloc.dart';
+import 'package:app/practiceOne/HomePageOne.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,87 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BloC Practice',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // 01
-  final counterBloc = CounterBloc();
-
-  // 05
-  @override
-  void dispose() {
-    counterBloc.closeTheStreamController();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Spacer(),
-            // 04
-            IconButton(
-              onPressed: () {
-                counterBloc.eventSink.add(UserCounterAction.Decre);
-              },
-              icon: Icon(Icons.remove),
-            ),
-
-            // 03
-            // [Here is the place where we are listening]
-            StreamBuilder(
-              initialData: 0,
-              stream: counterBloc.counterStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData)
-                  return Text(
-                    '${snapshot.data}',
-                  );
-                if (snapshot.hasError) return Text(snapshot.error.toString());
-                if (snapshot.connectionState == ConnectionState.active)
-                  return Text('Active');
-                else
-                  return Text('No data found');
-              },
-            ),
-
-            // 02
-            IconButton(
-              onPressed: () {
-                counterBloc.eventSink.add(UserCounterAction.Incre);
-              },
-              icon: Icon(Icons.add),
-            ),
-            Spacer(),
-            TextButton(
-                onPressed: () {
-                  counterBloc.eventSink.add(UserCounterAction.Reset);
-                },
-                child: Text('Reset'.toUpperCase())),
-          ],
-        ),
-      ),
+      home: HomePageOne(),
     );
   }
 }
---
+++ app/lib/practiceOne/HomePageOne.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import '../counter_bloc.dart';
+
+class HomePageOne extends StatefulWidget {
+  const HomePageOne({Key? key}) : super(key: key);
+
+  @override
+  _HomePageOneState createState() => _HomePageOneState();
+}
+
+class _HomePageOneState extends State<HomePageOne> {
+  // 01
+  final counterBloc = CounterBloc();
+
+  // 05
+  @override
+  void dispose() {
+    counterBloc.closeTheStreamController();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App with BloC'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            // 04
+            IconButton(
+              onPressed: () {
+                counterBloc.eventSink.add(UserCounterAction.Decre);
+              },
+              icon: Icon(Icons.remove),
+            ),
+
+            // 03
+            // [Here is the place where we are listening]
+            StreamBuilder(
+              initialData: 0,
+              stream: counterBloc.counterStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData)
+                  return Text(
+                    '${snapshot.data}',
+                  );
+                if (snapshot.hasError) return Text(snapshot.error.toString());
+                if (snapshot.connectionState == ConnectionState.active)
+                  return Text('Active');
+                else
+                  return Text('No data found');
+              },
+            ),
+
+            // 02
+            IconButton(
+              onPressed: () {
+                counterBloc.eventSink.add(UserCounterAction.Incre);
+              },
+              icon: Icon(Icons.add),
+            ),
+            Spacer(),
+            TextButton(
+              onPressed: () {
+                counterBloc.eventSink.add(UserCounterAction.Reset);
+              },
+              child: Text(
+                'Reset'.toUpperCase(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
