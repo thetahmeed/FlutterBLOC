03638f0605aba41cef10a96fbf435f1cc84a1ba0
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-06-12 10:45:02 +0600

    Sink :: StreamController :: Stream
    '_newsSink' here '_' means this is private

--- app/lib/practiceTwo/bloc/news_bloc.dart
+++ app/lib/practiceTwo/bloc/news_bloc.dart
@@ -1,3 +1,10 @@
 enum UserNewsAction {
   Read,
 }
+  final _stateStreamController = StreamController<List<Articles>?>();
+  StreamSink<List<Articles>?> get _newsSink => _stateStreamController.sink;
+  Stream<List<Articles>?> get newsStream => _stateStreamController.stream;
+
+  final _eventStreamController = StreamController<UserNewsAction>();
+  StreamSink<UserNewsAction> get eventSink => _eventStreamController.sink;
+  Stream<UserNewsAction> get _eventStream => _eventStreamController.stream;
