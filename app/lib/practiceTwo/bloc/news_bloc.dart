01dccd4aaf8314149806b56700a826de6d0b360e
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-06-12 10:46:51 +0600

    IGNORE:

--- app/lib/practiceTwo/bloc/news_bloc.dart
+++ app/lib/practiceTwo/bloc/news_bloc.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+import 'package:app/practiceTwo/api/apiCall.dart';
+import 'package:app/practiceTwo/model/articles.dart';
+
 enum UserNewsAction {
   Read,
 }
+
+class NewsBloc {
   final _stateStreamController = StreamController<List<Articles>?>();
   StreamSink<List<Articles>?> get _newsSink => _stateStreamController.sink;
   Stream<List<Articles>?> get newsStream => _stateStreamController.stream;
@@ -8,6 +14,7 @@ enum UserNewsAction {
   final _eventStreamController = StreamController<UserNewsAction>();
   StreamSink<UserNewsAction> get eventSink => _eventStreamController.sink;
   Stream<UserNewsAction> get _eventStream => _eventStreamController.stream;
+
   NewsBloc() {
     _eventStream.listen((event) async {
       if (event == UserNewsAction.Read) {
@@ -20,7 +27,9 @@ enum UserNewsAction {
       }
     });
   }
+
   void closeTheController() {
     _stateStreamController.close();
     _eventStreamController.close();
   }
+}
