78fb9f58b226f020b00d7d3fdfd5eb6051cbcf9d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-06-12 10:42:50 +0600

    User actions

--- app/lib/practiceTwo/constants/urls.dart
+++ app/lib/practiceTwo/constants/urls.dart
@@ -1,6 +1,6 @@
 class Urls {
   static String mainUrl =
-      'http://newsapi.org/v2/everything?q=google&apiKey=YOUR_KEY_HERE';
+      'http://newsapi.org/v2/everything?q=google&apiKey=0cd2f0b68fce4b6eb05c18f7dee3d530';
   //static String query = '';
   //static String apiKey = '';
 }
